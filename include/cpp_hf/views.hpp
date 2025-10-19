6a8101333aad6f94a7d4165c15ed3174a8d43cbc
Tobias Wolf <tobias.mr.wolf@gmail.com>
2025-10-19 02:54:58 -0500

    Refactor mdspan integration: remove detection logic, update C++ standard to 23, and adjust macOS deployment target

--- include/cpp_hf/views.hpp
+++ include/cpp_hf/views.hpp
@@ -1,22 +1,21 @@
 // views.hpp - mdspan aliases and helpers
 #pragma once
 
-#include "cpp_hf/hf_mdspan.hpp"
+#include <mdspan>
 #include <cstddef>
-#include <span> // for std::dynamic_extent
 
 namespace hf {
 
-// Use stdx::dynamic_extent uniformly; works with std or experimental mdspan
-using Ext2 = stdx::extents<std::size_t, stdx::dynamic_extent, stdx::dynamic_extent>;
-using Ext4 = stdx::extents<std::size_t, stdx::dynamic_extent, stdx::dynamic_extent,
-                                      stdx::dynamic_extent, stdx::dynamic_extent>;
+// Standard mdspan aliases
+using Ext2 = std::extents<std::size_t, std::dynamic_extent, std::dynamic_extent>;
+using Ext4 = std::extents<std::size_t, std::dynamic_extent, std::dynamic_extent,
+                                      std::dynamic_extent, std::dynamic_extent>;
 
 template <class T>
-using Grid2 = stdx::mdspan<T, Ext2, stdx::layout_right>;
+using Grid2 = std::mdspan<T, Ext2, std::layout_right>;
 
 template <class T>
-using Grid4 = stdx::mdspan<T, Ext4, stdx::layout_right>;
+using Grid4 = std::mdspan<T, Ext4, std::layout_right>;
 
 // Access helper to avoid depending on operator() availability across lib versions
 template <class Mdspan, class... Idx>
